37f040098f815d921502bbd78214f5d26de54aaf
richoandy <richoandy@gmail.com>
2018-04-12 14:57:09 +0700

    3

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -2,8 +2,14 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var userSchema = new Schema({
-    email: String,
-    password: String
+    email: {
+        type: String,
+        required: [true, 'email is required']
+    },
+    password: {
+        type: String,
+        required: [true, 'password is required']
+    }
 });
 
 var users = mongoose.model('users', userSchema);
